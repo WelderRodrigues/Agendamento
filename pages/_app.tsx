70a6321f37e2973325c6636c43e3ca1b1cc9db43
Welder Rodrigues <welder_15@hotmail.com>
2020-09-18 20:28:04 -0300

    Contact page
    Finalizada a pagina de contatos

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import ThemeContainer from '../contexts/theme/ThemeContainer'
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <ThemeContainer>
+    <Component {...pageProps}/>
+  </ThemeContainer>)
+}
+
+export default MyApp
