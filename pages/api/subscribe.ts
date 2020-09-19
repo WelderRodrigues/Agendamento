303ff59debce2a48187c9c81d4093520173507a0
Welder Rodrigues <welder_15@hotmail.com>
2020-09-19 20:43:40 -0300

    Finished function serveless local
    Finalizada a função serveless local para envio das informações para o banco de dados na nuvem

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,7 +1,42 @@
 import {NowRequest, NowResponse} from '@vercel/node'
+import { MongoClient, Db} from 'mongodb'
+import url from 'url'
 
-export default (request: NowRequest, response: NowResponse) => {
+let cachedDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+
+    if (cachedDb) {
+        return cachedDb;
+    }
+
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    const dbName = url.parse(uri).pathname.substr(1)
+
+    const db = client.db(dbName)
+
+    cachedDb = db;
+
+    return db
+}
+
+export default async (request: NowRequest, response: NowResponse) => {
     const { name, number, address } = request.body;
 
-    return response.json({msg: `Olá ${name} ${number} ${address}`})
+    const db = await connectToDatabase(process.env.MONGODB_URI)
+
+    const collection = db.collection('agendamentos')
+
+    await collection.insertOne({
+        name,
+        number,
+        address,
+        agendamentoAt: new Date()
+    })
+
+    return response.status(201).json({ Ok: true})
 }
